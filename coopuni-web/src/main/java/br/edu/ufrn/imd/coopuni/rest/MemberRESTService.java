b11b3e388d0ffa33bf076c5b2262511e9a11f47c
Bledson Kivy <bledson@gmail.com>
2015-09-13 19:53:43 -0300

    creating sign up page

---
+++ coopuni-web/src/main/java/br/edu/ufrn/imd/coopuni/rest/MemberRESTService.java
@@ -0,0 +1,22 @@
+//package br.edu.ufrn.imd.coopuni.rest;
+//
+//import br.edu.ufrn.imd.coopuni.service.MemberService;
+//
+//import javax.enterprise.context.RequestScoped;
+//import javax.inject.Inject;
+//import javax.ws.rs.Path;
+//import javax.xml.validation.Validator;
+//import java.util.logging.Logger;
+//
+//@Path("/members")
+//@RequestScoped
+//public class MemberRESTService {
+//  @Inject
+//  private Logger log;
+//
+//  @Inject
+//  private Validator validator;
+//
+//  @Inject
+//  private MemberService memberService;
+//}
